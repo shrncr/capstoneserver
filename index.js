c108f7dfaad750088c26d53e75b38308d2e7b9d3
shrncr <loveyawink@gmail.com>
2025-03-26 19:43:22 -0500

    .

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors({
     origin: '*',  // Allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    
 }));
 
 app.use(express.json());
