ac071f5bdaa949f872d5a772ac05b42b39c6f3b4
shrncr <loveyawink@gmail.com>
2025-03-30 20:50:43 -0500

    .

--- index.js
+++ index.js
@@ -500,6 +500,6 @@ app.post('/closeAttendenceSession', async (req, res) => {
         res.status(500).json({ success: false, message: 'Error recording attendance', error });
     }
 });
-const PORT = process.env.PORT |5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     
