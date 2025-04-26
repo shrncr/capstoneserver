4a231422bb01d5a3ad4a72cb0ad3aeb4cd7da6c2
shrncr <loveyawink@gmail.com>
2025-04-26 16:46:02 -0500

    added origin

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 const io = socketIo(server); 
 app.use(cors({
     origin: ['http://localhost:3000', 'chrome-extension://cldcoaaoanjlgjodnafeapnaommcmhie',  'chrome-extension://jabbmdnfncbfldfdeedhpnpbnljehpib', "http://127.0.0.1:5001",
-        'chrome-extension://dcbmdamicjlhpencmfggghalkhhaakka', 'chrome-extension://hpkghbcgocjbfkbglccebjcejcgeofom'
+        'chrome-extension://dcbmdamicjlhpencmfggghalkhhaakka', 'chrome-extension://hpkghbcgocjbfkbglccebjcejcgeofom', 'chrome-extension://dcbmdamicjlhpencmfggghalkhhaakka'
      ] ,//allow requests from frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization', 'User-Agent', 'Accept', 'Referer'], 
